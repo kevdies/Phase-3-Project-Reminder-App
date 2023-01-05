9e1f0be13dbffe4b322eb35512e0afbe9d193a39
Najma <najma.a16@gmail.com>
2023-01-04 19:48:08 -0600

    started setting up components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from'react';
+import Header from './Header.js'
+import Days from './Days.js'
+import Form from './Form.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>
+        
+        <header>TO DO LIST </header>
+
+      </Header>
+      <Form>
+        <p>Add New Task Here</p>
+        </Form>
+      <Days> 
+        <p>Days of the Week </p>
+        </Days>
     </div>
   );
 }
---
+++ src/Days.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Days(){
+
+
+
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Days;
